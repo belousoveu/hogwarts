81c6ecb5e242f104fd72cf190b1ccda06a48451c
Belousoveu <belousov.eu@gmail.com>
2024-11-06 21:07:31 +0300

    Initial commit

---
+++ src/test/java/com/github/belousoveu/hogwarts/HogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.belousoveu.hogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
