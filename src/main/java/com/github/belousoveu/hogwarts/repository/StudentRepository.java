e0a9e8c0d5fcace8949d6fdf5ada7ca940dade1a
Belousoveu <belousov.eu@gmail.com>
2024-12-09 16:03:38 +0300

    added 3 endpoints for: - get total number of students - get the average age of all students - get any(default - 5) last student sorting by id Queries have been created in the database to implement these methods

--- src/main/java/com/github/belousoveu/hogwarts/repository/StudentRepository.java
+++ src/main/java/com/github/belousoveu/hogwarts/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.github.belousoveu.hogwarts.repository;
 
 import com.github.belousoveu.hogwarts.model.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
@@ -16,4 +18,10 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     Collection<Student> findAllByAgeBetween(int minAge, int maxAge);
 
     Collection<Student> findAllByAgeIn(List<Integer> age);
+
+    @Query("SELECT AVG(s.age) FROM students s")
+    Double averageAge();
+
+    @Query(value = "SELECT * FROM (SELECT * FROM students s ORDER BY s.id DESC LIMIT :amount) AS subquery ORDER BY subquery.id ASC",  nativeQuery = true)
+    Collection<Student> getLastStudents(@Param("amount") long amount);
 }
